e07467d527d5cb24bfc0ec6619542584b8338fca
Anandhuyej <88794006+Anandhuyej@users.noreply.github.com>
2022-12-12 17:52:33 +0000

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1928568176"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "postgres://pwavfvkwvvdumy:f6ec843e80cbaceda24b921d2b57d76257546946cac020476473a4b3bc86734f@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/d602b4c559v41d")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Flud:Flud@cluster0.whmo0.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
